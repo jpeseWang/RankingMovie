45f366422cee288761a88114fd5cf63d7fb6df9d
leif.le <leif.le@avepoint.com>
2024-01-25 17:30:47 +0700

    models

---
+++ Ranking/ClientApp/src/components/RankItems.jsx
@@ -0,0 +1,25 @@
+﻿import React, { useState, useEffect } from 'react'
+
+const RankItems = () => {
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((res) => {
+                return res.json()
+            })
+            .then(data => {
+                setItems(data)
+            })
+    }, [])
+
+    return(
+        <main>
+            {(items != null) ? items.map((item) => <h3>{item.title}</h3>) : <>Loading...</>}
+
+        </main>
+    )
+}
+
+export default RankItems
